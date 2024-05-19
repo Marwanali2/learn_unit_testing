38348154187ab6707414bf6b78802dc61fa5c0ac
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-05-19 17:08:23 +0300

    Update user_repository.dart and user_repository_test.dart

--- test/user_repository_test.dart
+++ test/user_repository_test.dart
@@ -27,26 +27,7 @@ void main() {
                 'https://jsonplaceholder.typicode.com/users/1')).thenAnswer(
               (invocation) async => Response(
                 requestOptions: RequestOptions(path: ''),
-                data: { // الداتا اللي هترجعها ال API
-                  "id": 1,
-                  "name": "Leanne Graham",
-                  "username": "Bret",
-                  "email": "Sincere@april.biz",
-                  "address": {
-                    "street": "Kulas Light",
-                    "suite": "Apt. 556",
-                    "city": "Gwenborough",
-                    "zipcode": "92998-3874",
-                    "geo": {"lat": "-37.3159", "lng": "81.1496"}
-                  },
-                  "phone": "1-770-736-8031 x56442",
-                  "website": "hildegard.org",
-                  "company": {
-                    "name": "Romaguera-Crona",
-                    "catchPhrase": "Multi-layered client-server neural-net",
-                    "bs": "harness real-time e-markets"
-                  }
-                },
+                data:(await Dio().get('https://jsonplaceholder.typicode.com/users/1')).data, //الداتا اللي هترجعها ال API
                 statusCode: 200,
               ),
             );
